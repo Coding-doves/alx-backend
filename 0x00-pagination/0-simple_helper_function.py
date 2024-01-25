ee84f7ba67ab5ed3c9dcd4bf9c0db59e17c106f3
dove <obenedicta4@gmail.com>
2024-01-25 23:34:20 +0100

    task1

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -1,3 +1,19 @@
 #!/usr/bin/env python3
-''' comment '''
+'''
+return a tuple of size two containing a start index/item
+and an end index/item
+'''
 
+
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
+    '''
+    page: to open
+    page_size: number of item to display
+    '''
+    if page <= 0 or page_size <= 0:
+        return None  # Invalid input
+
+    start_index = (page - 1) * page_size
+    end_index = start_index + page_size - 1
+
+    return start_index, end_index
