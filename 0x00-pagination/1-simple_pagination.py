9c2fe492726602d692c97fb9157a2e4175800ecd
Ada <106437177+Coding-doves@users.noreply.github.com>
2024-01-27 13:46:43 +0100

    Update 1-simple_pagination.py

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -4,3 +4,16 @@ import csv
 import math
 from typing import List, Tuple
 
+
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
+    '''
+    page: to open
+    page_size: number of item to display
+    '''
+    if page <= 0 or page_size <= 0:
+        return None
+
+    start_index = (page - 1) * page_size
+    end_index = start_index + page_size
+
+    return start_index, end_index
