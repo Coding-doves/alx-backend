3030848e2bf01dcf2e70345376bda4fd77aa3714
Ada <106437177+Coding-doves@users.noreply.github.com>
2024-01-27 13:43:59 +0100

    Update 0-simple_helper_function.py

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -10,9 +10,9 @@ def index_range(page: int, page_size: int) -> Tuple[int, int]:
     '''
     page: to open
     page_size: number of item to display
-    
+    '''
     if page <= 0 or page_size <= 0:
-        return None '''
+        return None
 
     start_index = (page - 1) * page_size
     end_index = start_index + page_size - 1
