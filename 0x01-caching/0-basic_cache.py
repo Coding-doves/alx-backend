2dde34efb44242951faa5e0a714b3b031ad87a51
Ada <106437177+Coding-doves@users.noreply.github.com>
2024-01-31 22:42:02 +0100

    Update 0-basic_cache.py

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -18,6 +18,4 @@ class BasicCache(BaseCaching):
 
     def get(self, key):
         """get from cache"""
-        # if key is None or key not in self.cache_data:
-            # return None
         return self.cache_data.get(key, None)
