b8bc5884b478d9ae20f384939e81bf0407c659d6
dove <obenedicta4@gmail.com>
2024-01-31 21:27:47 +0100

    task 1

---
+++ 0x01-caching/0-basic_cache.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+""" BasicCache module
+"""
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class BasicCache(BaseCaching):
+    '''BasicCache'''
+
+    def put(self, key, item):
+        '''put'''
+        if key is None or item is None:
+            return
+        self.cache_data[key] = item
+
+    def get(self, key):
+        '''get'''
+        if key is None or key not in self.cache_data:
+            return None
+        return self.cache_data.get(key)
