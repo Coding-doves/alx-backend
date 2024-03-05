9d9d57c392dd56d7ddc6cfd51ee5555c7aa25687
dove <obenedicta4@gmail.com>
2024-03-05 23:00:53 +0100

    updating

---
+++ 0x03-queuing_system_in_js/0-redis_client.js
@@ -0,0 +1,15 @@
+// Import the required library
+import redis from 'redis';
+
+// Create a Redis client
+const client = redis.createClient();
+
+// Event listener for successful connection
+client.on('connect', () => {
+  console.log('Redis client connected to the server');
+});
+
+// Event listener for connection error
+client.on('error', (err) => {
+  console.error(`Redis client not connected to the server: ${err}`);
+});
